1343fcb5d06e45656ae6366bac00f141509fa6f2
Deepak Duvedi <dduvedi6@gmail.com>
2024-10-10 09:43:00 +0530

    Update Subtraction.java

--- Subtraction.java
+++ Subtraction.java
@@ -2,6 +2,6 @@ package com.operations.calculator;
 
 public class Subtraction implements CalculationStrategy {
     public double calculate(double a, double b) {
-        return a - b;
+        return a + b;
     }
 }
